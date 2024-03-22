228a32cd8499bbf8fd209592da35528c6df57d87
GOJuanIgnacio <nachobz200607@gmail.com>
2024-03-22 10:08:57 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Database, object, ref } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private database:Database, ) {
+
+
+  }
+  ngOnInit(): void {
+    const route = ref(this.database, '/casa/banio');
+    object(route).subscribe(attributes => {
+      const valores_db = attributes.snapshot.val();
+      console.log(valores_db);
+    });
+  }
+  
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBZOkT4zWx1f7e1izLbio2YekermKV8AW8",
+    authDomain: "rtdb-insum2.firebaseapp.com",
+    projectId: "rtdb-insum2",
+    storageBucket: "rtdb-insum2.appspot.com",
+    messagingSenderId: "846965313214",
+    appId: "1:846965313214:web:c64b34a0e21c06fa55d8ef"
+  }
 };
 
 /*
