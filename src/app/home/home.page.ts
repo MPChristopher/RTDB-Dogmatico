106c5ba1caf1831e1cafa1e6c4f577865d6e0c35
MPChristopher <papiarceus@gmail.com>
2024-03-29 17:05:41 -0500

    Second Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,18 +7,28 @@ import { Database, object, ref } from '@angular/fire/database';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
+  vidrioColores: { [key: string]: string } = {};
 
-  constructor(private database:Database, ) {
+  constructor(private database:Database, ) {}
 
+  ngOnInit() {
+    const baseRoute = '/casa/';
+    const cuartos = ['banio', 'bodega', 'cocina', 'dormitorio', 'dormitorio2', 'sala'];
 
-  }
-  ngOnInit(): void {
-    const route = ref(this.database, '/casa/banio');
-    object(route).subscribe(attributes => {
-      const valores_db = attributes.snapshot.val();
-      console.log(valores_db);
+    cuartos.forEach(cuarto => {
+      const route = ref(this.database, baseRoute + cuarto);
+      object(route).subscribe(attributes => {
+        const valores_db = attributes.snapshot.val();
+        this.vidrioColores[cuarto] = this.obtenerColorVidrio(valores_db);
+      });
     });
   }
   
-
+  obtenerColorVidrio(valores_db: any): string {
+    if (valores_db && valores_db['casa'] === true) {
+      return 'rgba(0, 255, 0, 0.5)';
+    } else {
+      return 'rgba(24, 209, 255, 0.5)';
+    }
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyBZOkT4zWx1f7e1izLbio2YekermKV8AW8",
     authDomain: "rtdb-insum2.firebaseapp.com",
-    projectId: "rtdb-insum2",
+    databaseURL: "https://rtdb-insum2-default-rtdb.firebaseio.com",
+    projectId: "rtdb-insum2", 
     storageBucket: "rtdb-insum2.appspot.com",
     messagingSenderId: "846965313214",
     appId: "1:846965313214:web:c64b34a0e21c06fa55d8ef"
