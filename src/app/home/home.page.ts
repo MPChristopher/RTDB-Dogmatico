fcc7d08cd977083f306300436a305987bfe50fe1
GOJuanIgnacio <nachobz200607@gmail.com>
2024-04-05 06:55:47 -0600

    Trabajo Realizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,28 +7,19 @@ import { Database, object, ref } from '@angular/fire/database';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-  vidrioColores: { [key: string]: string } = {};
+  valores_db:any={};
 
   constructor(private database:Database, ) {}
 
   ngOnInit() {
-    const baseRoute = '/casa/';
-    const cuartos = ['banio', 'bodega', 'cocina', 'dormitorio', 'dormitorio2', 'sala'];
-
-    cuartos.forEach(cuarto => {
-      const route = ref(this.database, baseRoute + cuarto);
+    const baseRoute = '/casa';
+    
+      const route = ref(this.database,baseRoute);
       object(route).subscribe(attributes => {
-        const valores_db = attributes.snapshot.val();
-        this.vidrioColores[cuarto] = this.obtenerColorVidrio(valores_db);
+        this.valores_db = attributes.snapshot.val();
+        console.log(this.valores_db);
       });
-    });
+    ;
   }
   
-  obtenerColorVidrio(valores_db: any): string {
-    if (valores_db && valores_db['casa'] === true) {
-      return 'rgba(0, 255, 0, 0.5)';
-    } else {
-      return 'rgba(24, 209, 255, 0.5)';
-    }
-  }
 }
